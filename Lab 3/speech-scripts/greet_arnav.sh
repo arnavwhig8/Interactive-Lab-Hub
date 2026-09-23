df307b50352cfb7a628dc462cd1160945f8865ab
Arnav Whig <aw966@cornell.edu>
2026-09-23 18:27:07 -0400

    add custom TTS greeting

--- Lab 3/speech-scripts/greet_arnav.sh
+++ Lab 3/speech-scripts/greet_arnav.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Hello Arnav, welcome back!" | python3 -m piper \
+echo "Hello Arnav and Shifen, welcome back!" | python3 -m piper \
   --model en_US-lessac-medium \
   --output_file greeting.wav
 
